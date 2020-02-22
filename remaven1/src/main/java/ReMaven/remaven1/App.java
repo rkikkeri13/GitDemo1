45466f0cc80e9c969be46a7e8833941415123e99
rkikkeri13 <kikkeri.rachana@gmail.com>
2020-02-22 12:04:26 -0800

    Committing everything

---
+++ remaven1/src/main/java/ReMaven/remaven1/App.java
@@ -0,0 +1,13 @@
+package ReMaven.remaven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
